9b59bb4bdaffd1ec9c5d3e0502f84695f8eaf3bb
DDevil NightRaid <ddevil@gmail.com>
2023-12-26 00:05:03 +0545

    fixedd some errors

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,9 +1,9 @@
-import { GetForms, GetFormStats } from "@/actions/form";
+import { GetFormStats, GetForms } from "@/actions/form";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Skeleton } from "@/components/ui/skeleton";
-import { ReactNode, Suspense, useEffect, useState } from "react";
+import { ReactNode, Suspense } from "react";
 import { LuView } from "react-icons/lu";
-import { FaEdit, FaWpforms } from "react-icons/fa";
+import { FaWpforms } from "react-icons/fa";
 import { HiCursorClick } from "react-icons/hi";
 import { TbArrowBounce } from "react-icons/tb";
 import { Separator } from "@/components/ui/separator";
@@ -13,7 +13,8 @@ import { Badge } from "@/components/ui/badge";
 import { formatDistance } from "date-fns";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import { ArrowRightIcon } from "@radix-ui/react-icons";
+import { BiRightArrowAlt } from "react-icons/bi";
+import { FaEdit } from "react-icons/fa";
 
 export default function Home() {
   return (
@@ -22,11 +23,15 @@ export default function Home() {
         <CardStatsWrapper />
       </Suspense>
       <Separator className="my-6" />
-      <h2 className="text-4xl font-bold col-span-2">Your Forms</h2>
+      <h2 className="text-4xl font-bold col-span-2">Your forms</h2>
       <Separator className="my-6" />
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7">
+      <div className="grid gric-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         <CreateFormBtn />
-        <Suspense fallback={[1,2,3,4].map(el => <FormCardSkeleton key={el} />)}>
+        <Suspense
+          fallback={[1, 2, 3, 4].map((el) => (
+            <FormCardSkeleton key={el} />
+          ))}
+        >
           <FormCards />
         </Suspense>
       </div>
@@ -50,36 +55,39 @@ function StatsCards(props: StatsCardProps) {
   return (
     <div className="w-full pt-8 gap-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
       <StatsCard
-        title={"Total Visits"}
+        title="Total visits"
         icon={<LuView className="text-blue-600" />}
-        helperText={"All time for visits"}
+        helperText="All time form visits"
         value={data?.visits.toLocaleString() || ""}
         loading={loading}
-        className={"shadow-md shadow-blue-600"}
+        className="shadow-md shadow-blue-600"
       />
+
       <StatsCard
-        title={"Total Submitions"}
+        title="Total submissions"
         icon={<FaWpforms className="text-yellow-600" />}
-        helperText={"All time form submissions"}
+        helperText="All time form submissions"
         value={data?.submissions.toLocaleString() || ""}
         loading={loading}
-        className={"shadow-md shadow-blue-600"}
+        className="shadow-md shadow-yellow-600"
       />
+
       <StatsCard
-        title={"Total Submition Rate"}
+        title="Submission rate"
         icon={<HiCursorClick className="text-green-600" />}
-        helperText={"Visits that result on submition"}
+        helperText="Visits that result in form submission"
         value={data?.submissionRate.toLocaleString() + "%" || ""}
         loading={loading}
-        className={"shadow-md shadow-green-600"}
+        className="shadow-md shadow-green-600"
       />
+
       <StatsCard
-        title={"Bounce Rate"}
+        title="Bounce rate"
         icon={<TbArrowBounce className="text-red-600" />}
-        helperText={"Customer left"}
-        value={data?.submissions.toLocaleString() + "%" || ""}
+        helperText="Visits that leaves without interacting"
+        value={data?.submissionRate.toLocaleString() + "%" || ""}
         loading={loading}
-        className={"shadow-md shadow-red-600"}
+        className="shadow-md shadow-red-600"
       />
     </div>
   );
@@ -87,18 +95,18 @@ function StatsCards(props: StatsCardProps) {
 
 export function StatsCard({
   title,
+  value,
   icon,
   helperText,
-  value,
   loading,
   className,
 }: {
   title: string;
-  icon: ReactNode;
-  helperText: string;
   value: string;
-  loading: boolean;
+  helperText: string;
   className: string;
+  loading: boolean;
+  icon: ReactNode;
 }) {
   return (
     <Card className={className}>
@@ -110,7 +118,7 @@ export function StatsCard({
         <div className="text-2xl font-bold">
           {loading && (
             <Skeleton>
-              <span>0</span>
+              <span className="opacity-0">0</span>
             </Skeleton>
           )}
           {!loading && value}
@@ -121,74 +129,63 @@ export function StatsCard({
   );
 }
 
-
-function FormCardSkeleton(){
-  return <Skeleton className="border-2 border-primary-/20 h-[190px] w-full"/>
+function FormCardSkeleton() {
+  return <Skeleton className="border-2 border-primary-/20 h-[190px] w-full" />;
 }
 
-async function FormCards(){
+async function FormCards() {
   const forms = await GetForms();
-  return <>
-  {forms.map(form => (
-    <FormCard key={form.id} form={form}/>
-  ))}
-  </>
+  return (
+    <>
+      {forms.map((form) => (
+        <FormCard key={form.id} form={form} />
+      ))}
+    </>
+  );
 }
 
-
-function FormCard({form}: {
-  form: Form
-}){
-  return <Card>
-    <CardHeader>
-      <CardTitle className="flex items-center justify-between gap-2">
-        <span className="truncate font-bold">{form.name}</span>
-        {
-          form.published ? (<Badge>Published</Badge>)
-          : (<Badge variant={'destructive'}>Draft</Badge>)
-        }
-      </CardTitle>
-      <CardDescription className="flex items-center justify-between text-muted-foreground text-ms">
-        {formatDistance(form.createdAt, new Date, {
-          addSuffix: true,
-        })}
-        {
-          form.published && (
+function FormCard({ form }: { form: Form }) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle className="flex items-center gap-2 justify-between">
+          <span className="truncate font-bold">{form.name}</span>
+          {form.published && <Badge>Published</Badge>}
+          {!form.published && <Badge variant={"destructive"}>Draft</Badge>}
+        </CardTitle>
+        <CardDescription className="flex items-center justify-between text-muted-foreground text-sm">
+          {formatDistance(form.createdAt, new Date(), {
+            addSuffix: true,
+          })}
+          {form.published && (
             <span className="flex items-center gap-2">
-              <LuView calcMode={'text-muted-foreground'} />
+              <LuView className="text-muted-foreground" />
               <span>{form.visits.toLocaleString()}</span>
-              <FaWpforms calcMode={'text-muted-foreground'} />
+              <FaWpforms className="text-muted-foreground" />
               <span>{form.submissions.toLocaleString()}</span>
             </span>
-          )
-        }
-      </CardDescription>
-    </CardHeader>
-    <CardContent className="h-[20px] truncate text-sm text-muted-foreground">
-      {form.description || "No description"}
-    </CardContent>
-    <CardFooter>
-      {form.published && (
-        <Button
-          asChild
-          className="w-full mt-2 text-md gap-4"
-        >
-          <Link href={`/forms/${form.id}`}>
-            View Submissions <ArrowRightIcon />
-          </Link>
-        </Button>
-      )}
-      {!form.published && (
-        <Button
-          asChild
-          variant={'secondary'}
-          className="w-full mt-2 text-md gap-4"
-        >
-          <Link href={`/builder/${form.id}`}>
-            Edit form <FaEdit />
-          </Link>
-        </Button>
-      )}
-    </CardFooter>
-  </Card>
+          )}
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="h-[20px] truncate text-sm text-muted-foreground">
+        {form.description || "No description"}
+      </CardContent>
+      <CardFooter>
+        {form.published && (
+          <Button asChild className="w-full mt-2 text-md gap-4">
+            <Link href={`/forms/${form.id}`}>
+              View submissions <BiRightArrowAlt />
+            </Link>
+          </Button>
+        )}
+        {!form.published && (
+          <Button asChild variant={"secondary"} className="w-full mt-2 text-md gap-4">
+            <Link href={`/builder/${form.id}`}>
+              Edit form <FaEdit />
+            </Link>
+          </Button>
+        )}
+      </CardFooter>
+    </Card>
+  );
 }
--- components/DragOverlayWrapper.tsx
+++ components/DragOverlayWrapper.tsx
@@ -1,63 +1,52 @@
-import { Active, DragOverlay, useDndMonitor } from '@dnd-kit/core';
-import { useState } from 'react';
-import { ElementsType, FormElements } from './FormElements';
-import useDesigner from './hooks/useDesigner';
-import { SidebarBtnElementDragOverlay } from './SidebarBtnElement';
+import { Active, DragOverlay, useDndMonitor } from "@dnd-kit/core";
+import React, { useState } from "react";
+import { SidebarBtnElementDragOverlay } from "./SidebarBtnElement";
+import { ElementsType, FormElements } from "./FormElements";
+import useDesigner from "./hooks/useDesigner";
 
-const DragOverlayWrapper = () => {
+function DragOverlayWrapper() {
+  const { elements } = useDesigner();
+  const [draggedItem, setDraggedItem] = useState<Active | null>(null);
 
-    const { elements } = useDesigner();
+  useDndMonitor({
+    onDragStart: (event) => {
+      setDraggedItem(event.active);
+    },
+    onDragCancel: () => {
+      setDraggedItem(null);
+    },
+    onDragEnd: () => {
+      setDraggedItem(null);
+    },
+  });
 
-    const [draggedItem, setDraggeditem] = useState<Active | null>(null);
-    useDndMonitor({
-        onDragStart: (event) => {
-            console.log("Drag item", event)
-        }
-    })
+  if (!draggedItem) return null;
 
-    useDndMonitor({
-        onDragStart: (event) => {
-            setDraggeditem(event.active);
-        },
-        onDragCancel: (event) => {
-            setDraggeditem(null);
-        },
-        onDragEnd: (event) => {
-            setDraggeditem(null);
-        }
-    })
+  let node = <div>No drag overlay</div>;
+  const isSidebarBtnElement = draggedItem.data?.current?.isDesignerBtnElement;
 
-    if (!draggedItem) return null;
-    let node = <div>
-        No drag overlay
-    </div>
-    const isSidebarBtnElement = draggedItem.data?.current?.isDesignerBtnElement;
+  if (isSidebarBtnElement) {
+    const type = draggedItem.data?.current?.type as ElementsType;
+    node = <SidebarBtnElementDragOverlay formElement={FormElements[type]} />;
+  }
 
-    if (isSidebarBtnElement) {
-        const type = draggedItem.data?.current?.type as ElementsType;
-        node = <SidebarBtnElementDragOverlay formElement={FormElements[type]} />
+  const isDesignerElement = draggedItem.data?.current?.isDesignerElement;
+  if (isDesignerElement) {
+    const elementId = draggedItem.data?.current?.elementId;
+    const element = elements.find((el) => el.id === elementId);
+    if (!element) node = <div>Element not found!</div>;
+    else {
+      const DesignerElementComponent = FormElements[element.type].designerComponent;
+
+      node = (
+        <div className="flex bg-accent border rounded-md h-[120px] w-full py-2 px-4 opacity-80 pointer pointer-events-none">
+          <DesignerElementComponent elementInstance={element} />
+        </div>
+      );
     }
+  }
 
-    const isDesignerElement = draggedItem.data?.current?.isDesignerElement;
-
-    if (isDesignerElement) {
-        const elementId = draggedItem.data?.current?.elementId;
-        const element = elements.find(el => el.id === elementId);
-
-        if (!element) {
-            node = <div>Element not Found!</div>
-        } else {
-            const DesignerElementComponent = FormElements[element.type].designerComponent;
-            node = <div className='flex bg-accent border rounded-md w-full h-[120px] py-2 px-4 opacity-80 pointer-events-none'>
-                <DesignerElementComponent elementInstance={element} />
-            </div>
-        }
-    }
-    return (
-        <DragOverlay>
-            {node}
-        </DragOverlay>
-    )
+  return <DragOverlay>{node}</DragOverlay>;
 }
 
-export default DragOverlayWrapper
+export default DragOverlayWrapper;
